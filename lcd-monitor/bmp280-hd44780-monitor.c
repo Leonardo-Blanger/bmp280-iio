37c396b54de8b7ddf3712390807c779439deb33e
Leonardo Blanger <leonardoblanger@gmail.com>
2025-03-25 10:30:47 +0800

    Add method docstrings
    Signed-off-by: Leonardo Blanger <leonardoblanger@gmail.com>

--- lcd-monitor/bmp280-hd44780-monitor.c
+++ lcd-monitor/bmp280-hd44780-monitor.c
@@ -23,10 +23,17 @@ MODULE_DESCRIPTION("BMP280 live monitoring module "
  */
 MODULE_SOFTDEP("pre: bmp280-iio");
 
+/**
+ * Monitor context structure.
+ *
+ * There is one instance of this allocated for each probed driver.
+ */
 struct bmp280_hd44780_monitor {
+  // References to BMP280 IIO channels
   struct iio_channel *temperature_channel;
   struct iio_channel *pressure_channel;
-  struct delayed_work work;
+  // Work structure for the periodic data refresh
+  struct delayed_work dwork;
 };
 
 #define REFRESH_PERIOD_SEC 2
@@ -38,6 +45,12 @@ static void bmp280_hd44780_monitor_work(struct work_struct *work) {
   schedule_delayed_work(dwork, delay);
 }
 
+/**
+ * Monitor platform driver probe method.
+ *
+ * Allocates an instance of our monitor, retrieves references to the BMP280 IIO
+ * channels, and start our worker thread (as a system default workqueue entry).
+ */
 static int bmp280_hd44780_monitor_probe(struct platform_device *pdev) {
   pr_info("Probing bmp280-hd44780-monitor platform driver.\n");
   // Allocate driver context instance
@@ -63,9 +76,9 @@ static int bmp280_hd44780_monitor_probe(struct platform_device *pdev) {
     return PTR_ERR(monitor->pressure_channel);
   }
   // Set up workqueue entry for our running worker function
-  INIT_DELAYED_WORK(&monitor->work, bmp280_hd44780_monitor_work);
+  INIT_DELAYED_WORK(&monitor->dwork, bmp280_hd44780_monitor_work);
   unsigned long delay = msecs_to_jiffies(REFRESH_PERIOD_SEC * 1000);
-  if (!schedule_delayed_work(&monitor->work, delay)) {
+  if (!schedule_delayed_work(&monitor->dwork, delay)) {
     pr_err("Failed to schedule worker thread. Aborting probe.\n");
     // TODO: Check which error code I should return here
     return -EFAULT;
@@ -76,10 +89,20 @@ static int bmp280_hd44780_monitor_probe(struct platform_device *pdev) {
   return 0;
 }
 
+/**
+ * Monitor platform driver remove function.
+ *
+ * Stops the driver worker thread.
+ */
 static void bmp280_hd44780_monitor_remove(struct platform_device *pdev) {
   pr_info("Successfully removed bmp280-hd44780-monitor platform driver.\n");
 }
 
+/**
+ * Device Tree based matching ids (OF = open firmware).
+ *
+ * Used for auto loading the kernel module, and for driver matching.
+ */
 static const struct of_device_id bmp280_hd44780_monitor_of_driver_ids[] = {
   {
     .compatible = "leonardo,bmp280-hd44780-monitor",
@@ -87,6 +110,9 @@ static const struct of_device_id bmp280_hd44780_monitor_of_driver_ids[] = {
   { /* sentinel */ },
 };
 
+/**
+ * HD44780 LCD monitor platform driver for the BMP280 IIO driver.
+ */
 static struct platform_driver bmp280_hd44780_monitor_driver = {
   .probe = bmp280_hd44780_monitor_probe,
   .remove_new = bmp280_hd44780_monitor_remove,
@@ -96,6 +122,11 @@ static struct platform_driver bmp280_hd44780_monitor_driver = {
   },
 };
 
+/**
+ * Module init function.
+ *
+ * Registers the platform driver.
+ */
 static int __init bmp280_hd44780_monitor_init(void) {
   int status = platform_driver_register(&bmp280_hd44780_monitor_driver);
   if (status) {
@@ -107,6 +138,11 @@ static int __init bmp280_hd44780_monitor_init(void) {
   return 0;
 }
 
+/**
+ * Module exit function.
+ *
+ * Unregisters the platform driver registered during init.
+ */
 static void __exit bmp280_hd44780_monitor_exit(void) {
   platform_driver_unregister(&bmp280_hd44780_monitor_driver);
   pr_info("Removed bmp280-hd44780-monitor.\n");
