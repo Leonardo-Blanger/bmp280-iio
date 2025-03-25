b68036f6e30a2c661590074267b105595fb2f9c9
Leonardo Blanger <leonardoblanger@gmail.com>
2025-03-25 08:34:12 +0800

    Probing and removing platform driver
    Signed-off-by: Leonardo Blanger <leonardoblanger@gmail.com>

--- lcd-monitor/bmp280-hd44780-monitor.c
+++ lcd-monitor/bmp280-hd44780-monitor.c
@@ -1,8 +1,11 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/init.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/platform_device.h>
 #include <linux/printk.h>
+#include <linux/of.h>
 
 MODULE_AUTHOR("Leonardo Blanger");
 MODULE_LICENSE("GPL");
@@ -14,12 +17,44 @@ MODULE_DESCRIPTION("BMP280 live monitoring module "
  */
 MODULE_SOFTDEP("pre: bmp280-iio");
 
+static const struct of_device_id bmp280_hd44780_monitor_of_driver_ids[] = {
+  {
+    .compatible = "leonardo,bmp280-hd44780-monitor",
+  },
+  { /* sentinel */ },
+};
+
+static int bmp280_hd44780_monitor_probe(struct platform_device *pdev) {
+  pr_info("Successfully probed bmp280-hd44780-monitor platform driver.\n");
+  return 0;
+}
+
+static void bmp280_hd44780_monitor_remove(struct platform_device *pdev) {
+  pr_info("Successfully removed bmp280-hd44780-monitor platform driver.\n");
+}
+
+static struct platform_driver bmp280_hd44780_monitor_driver = {
+  .probe = bmp280_hd44780_monitor_probe,
+  .remove_new = bmp280_hd44780_monitor_remove,
+  .driver = {
+    .name = "bmp280-hd44780-monitor",
+    .of_match_table = of_match_ptr(bmp280_hd44780_monitor_of_driver_ids),
+  },
+};
+
 static int __init bmp280_hd44780_monitor_init(void) {
+  int status = platform_driver_register(&bmp280_hd44780_monitor_driver);
+  if (status) {
+    pr_err("Failed to register bmp280-hd44780-monitor platform driver: %d\n",
+	   status);
+    return status;
+  }
   pr_info("Loaded bmp280-hd44780-monitor.\n");
   return 0;
 }
 
 static void __exit bmp280_hd44780_monitor_exit(void) {
+  platform_driver_unregister(&bmp280_hd44780_monitor_driver);
   pr_info("Removed bmp280-hd44780-monitor.\n");
 }
 
