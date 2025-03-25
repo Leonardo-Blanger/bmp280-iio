cdeec163e6196bf202f8f400905fc23ab1c9a220
Leonardo Blanger <leonardoblanger@gmail.com>
2025-03-25 09:41:27 +0800

    Retrieving IIO channels in the probe function
    Signed-off-by: Leonardo Blanger <leonardoblanger@gmail.com>

--- lcd-monitor/bmp280-hd44780-monitor.c
+++ lcd-monitor/bmp280-hd44780-monitor.c
@@ -1,5 +1,9 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
+#include <linux/device.h>
+#include <linux/err.h>
+#include <linux/errno.h>
+#include <linux/iio/consumer.h>
 #include <linux/init.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
@@ -17,14 +21,37 @@ MODULE_DESCRIPTION("BMP280 live monitoring module "
  */
 MODULE_SOFTDEP("pre: bmp280-iio");
 
-static const struct of_device_id bmp280_hd44780_monitor_of_driver_ids[] = {
-  {
-    .compatible = "leonardo,bmp280-hd44780-monitor",
-  },
-  { /* sentinel */ },
+struct bmp280_hd44780_monitor {
+  struct iio_channel *temperature_channel;
+  struct iio_channel *pressure_channel;
 };
 
 static int bmp280_hd44780_monitor_probe(struct platform_device *pdev) {
+  pr_info("Probing bmp280-hd44780-monitor platform driver.\n");
+  // Allocate driver context instance
+  struct bmp280_hd44780_monitor *monitor =
+    devm_kzalloc(&pdev->dev, sizeof(struct bmp280_hd44780_monitor), GFP_KERNEL);
+  if (!monitor) {
+    return -ENOMEM;
+  }
+  // Attempt to retrieve temperature channel as a device property
+  monitor->temperature_channel =
+    devm_iio_channel_get(&pdev->dev, "temperature");
+  if (IS_ERR(monitor->temperature_channel)) {
+    pr_err("Failed to acquire IIO temperature channel with error %ld. "
+	   "Aborting probe.\n", PTR_ERR(monitor->temperature_channel));
+    return PTR_ERR(monitor->temperature_channel);
+  }
+  // Attempt to retrieve pressure channel as a device property
+  monitor->pressure_channel =
+    devm_iio_channel_get(&pdev->dev, "pressure");
+  if (IS_ERR(monitor->pressure_channel)) {
+    pr_err("Failed to acquire IIO pressure channel with error %ld. "
+	   "Aborting probe.\n", PTR_ERR(monitor->pressure_channel));
+    return PTR_ERR(monitor->pressure_channel);
+  }
+  // Make our context structure available from the platform driver
+  platform_set_drvdata(pdev, monitor);
   pr_info("Successfully probed bmp280-hd44780-monitor platform driver.\n");
   return 0;
 }
@@ -32,6 +59,13 @@ static int bmp280_hd44780_monitor_probe(struct platform_device *pdev) {
 static void bmp280_hd44780_monitor_remove(struct platform_device *pdev) {
   pr_info("Successfully removed bmp280-hd44780-monitor platform driver.\n");
 }
+
+static const struct of_device_id bmp280_hd44780_monitor_of_driver_ids[] = {
+  {
+    .compatible = "leonardo,bmp280-hd44780-monitor",
+  },
+  { /* sentinel */ },
+};
 
 static struct platform_driver bmp280_hd44780_monitor_driver = {
   .probe = bmp280_hd44780_monitor_probe,
